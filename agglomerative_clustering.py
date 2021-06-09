d94fb002ba11aa167b024a174dc968002c0d0a3c
yuval <yuval-livne@campus.technion.ac.il>
2021-06-09 11:00:36 +0300

    9.6

--- agglomerative_clustering.py
+++ agglomerative_clustering.py
@@ -8,13 +8,14 @@ class AgglomerativeClustering:
         self.link = link
         self.clusters = [cluster.Cluster(sample.s_id, [sample]) for sample in samples]
 
-    def compute_silhouette(self):
+    def compute_silhouette(self, matrix):
         sil_dict = {}
         for clus in self.clusters:
             for sample in clus.samples:
                 sum_dist = 0
                 for other in clus.samples:
-                    sum_dist += sample.compute_euclidean_distance(other)
+                    # sum_dist += sample.compute_euclidean_distance(other)
+                    sum_dist += matrix[(sample, other)]
                 sil_in = float(sum_dist/(len(clus.samples - 1)))
 
                 dist_to_clus = []
@@ -23,7 +24,8 @@ class AgglomerativeClustering:
                         continue
                     sum_dist = 0
                     for other in other_clus.samples:
-                        sum_dist += sample.compute_euclidean_distance(other)
+                        sum_dist += matrix[(sample, other)]
+                        # sum_dist += sample.compute_euclidean_distance(other)
                     dist_to_clus.append(float(sum_dist / (len(other_clus.samples))))
                 sil_out = min(dist_to_clus)
                 sil_dict[sample.s_id] = float((sil_out - sil_in) / (max(sil_out,sil_in)))
@@ -31,8 +33,8 @@ class AgglomerativeClustering:
         return sil_dict
 
 
-    def compute_summery_silhouette(self):
-        samples_dict = self.compute_silhouette()
+    def compute_summery_silhouette(self, matrix):
+        samples_dict = self.compute_silhouette(matrix)
         sum_total=0
         clus_dict={}
         for clus in self.clusters:
@@ -64,49 +66,91 @@ class AgglomerativeClustering:
                         if sample.s_id > other.s_id:
                             continue
                         if sample.label != other.label:
-                            TN+=1
+                            TN += 1
 
         return float((TP+TN)/number_of_pairs)
 
-    def compute_dist_clusters(self, merged_id):
+    def compute_dist_clusters(self, merged_id, matrix):
         dist_dict = {}
         for clus in self.clusters:
             for other_clus in self.clusters:
                 if (clus.c_id == merged_id or other_clus.c_id == merged_id) or (merged_id == -1):
                     if (clus.c_id > other_clus.c_id):
-                        dist_dict[(clus.c_id, other_clus.c_id)] = self.link.compute(clus, other_clus)
+                        dist_dict[(clus.c_id, other_clus.c_id)] = self.link.compute(clus, other_clus, matrix)
         return dist_dict
+
+    def matrix_dist(self):
+        matrix_dic={}
+        for clus in self.clusters:
+            for other_clus in self.clusters:
+                if (clus.c_id > other_clus.c_id):
+                    for sample in clus.samples:
+                        for other_sample in other_clus.samples:
+                            if (sample.s_id > other_sample.s_id):
+                                matrix_dic[(sample, other_sample)] = sample.compute_euclidean_distance(other_sample)
+        return matrix_dic
 
 
     def run(self, max_clusters):
+        print(self.clusters[0])
+        del self.clusters[0]
+        print(self.clusters[0])
+        sample_dist_matrix = self.matrix_dist()
         self.link.print_link()
-        merged_id = -1
-        dist_matrix = self.compute_dist_clusters(merged_id)
-        first_id = 0
-        second_id = 0
+        # merged_id = -1
+        # clus_dist_matrix = self.compute_dist_clusters(merged_id, sample_dist_matrix)
+        # first_id = 0
+        # second_id = 0
         first_clus = self.clusters[0]
         second_clus = self.clusters[0]
-        min_dist = dist_matrix[list(dist_matrix.keys())[0]]
+        min_dist = sample_dist_matrix[list(sample_dist_matrix.keys())[0]]
         while (len(self.clusters) > max_clusters):
-            for key in dist_matrix.keys():
-                if dist_matrix[key] < min_dist:
-                    first_id = key[0]
-                    second_id = key[1]
-                    min_dist = dist_matrix[key]
             for clus in self.clusters:
-                if clus.c_id == first_id:
-                    first_clus = clus
-                if clus.c_id == second_id:
-                    second_clus = clus
-
-            if first_id > second_id:
-                merged_id = second_id
-            else:
-                merged_id = first_id
+                for other_clus in self.clusters:
+                    if (clus.c_id > other_clus.c_id):
+                        clus_dist = self.link.compute(clus, other_clus, sample_dist_matrix)
+                        if clus_dist < min_dist:
+                            min_dist = clus_dist
+                            if clus.c_id > other_clus.c_id:
+                                first_clus = other_clus
+                                second_clus = clus
+                            else:
+                                first_clus = clus
+                                second_clus = other_clus
             first_clus.merge(second_clus)
-            dist_matrix = self.compute_dist_clusters(merged_id)
+            min_dist = max(sample_dist_matrix.values())
 
-        sum_sil = self.compute_summery_silhouette()
+            # for sample in clus.samples:
+                        #     for other_sample in other_clus.samples:
+                        #         if (sample.s_id > other_sample.s_id):
+                        #             if (sample_dist_matrix[(sample.s_id, other_sample.s_id)] < min_dist):
+                        #                 min_dist = sample_dist_matrix[(sample.s_id, other_sample.s_id)]
+                        #                 if clus.c_id > other_clus.c_id:
+                        #                     other_clus.merge(clus)
+                        #                     # merged_id = other_clus.c_id
+                        #                 else:
+                        #                     clus.merge(other_clus)
+                        #                     # merged_id = clus.c_id
+
+            # first_clus.merge(second_clus)
+            # for key in clus_dist_matrix.keys():
+            #     if clus_dist_matrix[key] < min_dist:
+            #         first_id = key[0]
+            #         second_id = key[1]
+            #         min_dist = clus_dist_matrix[key]
+            # for clus in self.clusters:
+            #     if clus.c_id == first_id:
+            #         first_clus = clus
+            #     if clus.c_id == second_id:
+            #         second_clus = clus
+            #
+            # if first_id > second_id:
+            #     merged_id = second_id
+            # else:
+            #     merged_id = first_id
+            # clus_dist_matrix = self.compute_dist_clusters(merged_id, sample_dist_matrix)
+
+        sum_sil = self.compute_summery_silhouette(sample_dist_matrix)
         for clus in self.clusters:
             clus.print_details(sum_sil[clus.c_id])
         print(f'Whole data: silhouette = {sum_sil[0]}, RI = {self.compute_rand_index()}')
--- link.py
+++ link.py
@@ -9,11 +9,13 @@ class Link():
 
 class SingleLink(Link):
 
-    def compute(self, cluster, other):
-        min = cluster.samples[0].compute_euclidean_distance(other.samples[0])
+    def compute(self, cluster, other, matrix):
+        min=matrix[(cluster.samples[0], other.samples[0])]
+        # min = cluster.samples[0].compute_euclidean_distance(other.samples[0])
         for sample_cluster in cluster.samples:
             for sample_other in other.samples:
-                current_dist = sample_cluster.compute_euclidean_distance(sample_other)
+                current_dist=matrix[(sample_cluster, sample_other)]
+                # current_dist = sample_cluster.compute_euclidean_distance(sample_other)
                 if (current_dist < min):
                     min = current_dist
         return min
@@ -28,11 +30,13 @@ class CompleteLink(Link):
     def print_link():
         print("complete link")
 
-    def compute(self, cluster, other):
-        max = cluster.samples[0].compute_euclidean_distance(other.samples[0])
+    def compute(self, cluster, other, matrix):
+        # max = cluster.samples[0].compute_euclidean_distance(other.samples[0])
+        max = matrix[(cluster.samples[0], other.samples[0])]
         for sample_cluster in cluster.samples:
             for sample_other in other.samples:
-                current_dist = sample_cluster.compute_euclidean_distance(sample_other)
+                current_dist=matrix[(sample_cluster, sample_other)]
+                # current_dist = sample_cluster.compute_euclidean_distance(sample_other)
                 if (current_dist > max):
                     max = current_dist
         return max
