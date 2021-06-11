b2dd9aa40aedc97fe79a1e9cfc3990080ccea4a8
Amit Frechter <amitfrework@gmail.com>
2021-06-11 14:20:32 +0300

    Finished documentation and small corrections

--- agglomerative_clustering.py
+++ agglomerative_clustering.py
@@ -10,32 +10,36 @@ class AgglomerativeClustering:
         self.clusters = [cluster.Cluster(sample.s_id, [sample]) for sample in samples]
 
     def compute_silhouette(self, matrix):
+        """
+        computes silhouette value(float) of every sample in the dataset
+        :param matrix: distance matrix between samples in the data(dictionary)
+        :return: returns a dictionary of sample and its silhouette value(dictionary)
+        """
         sil_dict = {}
         for clus in self.clusters:
-            for sample in clus.samples:
+            for sample in clus.samples:  # iterate over every sample in the dataset
                 sum_dist = 0
-                for other in clus.samples:
+                for other in clus.samples:  # iterate over every other sample in the same cluster
                     if sample.s_id > other.s_id:
-                        # sum_dist += sample.compute_euclidean_distance(other)
                         sum_dist += matrix[(sample.s_id, other.s_id)]
                     elif sample.s_id < other.s_id:
                         sum_dist += matrix[(other.s_id, sample.s_id)]
                 if len(clus.samples) > 1:
                     sil_in = float(sum_dist / (len(clus.samples) - 1))
-                else:
+                else:  # when a sample is the only one in the cluster, its silhouette in value is 0
                     sil_in = 0
 
                 dist_to_clus = []
-                for other_clus in self.clusters:
+                for other_clus in self.clusters:  # iterate over other clusters
                     if other_clus == clus:
                         continue
                     sum_dist = 0
-                    for other in other_clus.samples:
+                    for other in other_clus.samples:  # iterate over every other sample in other clusters
                         if sample.s_id > other.s_id:
                             sum_dist += matrix[(sample.s_id, other.s_id)]
                         else:
                             sum_dist += matrix[(other.s_id, sample.s_id)]
-                        # sum_dist += sample.compute_euclidean_distance(other)
+                    # save the distance from the sample to every other cluster
                     dist_to_clus.append(float(sum_dist / (len(other_clus.samples))))
                 sil_out = min(dist_to_clus)
                 if sil_in == 0:
@@ -46,7 +50,12 @@ class AgglomerativeClustering:
         return sil_dict
 
     def compute_summery_silhouette(self, matrix):
-        samples_dict = self.compute_silhouette(matrix)
+        """
+        computes silhouette value(float) of every cluster and for the whole dataset
+        :param matrix: distance matrix between samples in the data(dictionary)
+        :return: returns a dictionary of cluster(or the whole dataset) and its silhouette value(dictionary)
+        """
+        samples_dict = self.compute_silhouette(matrix)  # holds the samples silhouette dictionary
         sum_total = 0
         clus_dict = {}
         for clus in self.clusters:
@@ -54,112 +63,78 @@ class AgglomerativeClustering:
             for sample in clus.samples:
                 sum_clus += samples_dict[sample.s_id]
                 sum_total += samples_dict[sample.s_id]
-
-            clus_dict[clus.c_id] = round(float(sum_clus) / len(clus.samples), 3)
-        clus_dict[0] = round(float(sum_total / len(self.samples)), 3)
+            clus_dict[clus.c_id] = round(float(sum_clus) / len(clus.samples), 3)  # save each cluster's silhouette
+        clus_dict[0] = round(float(sum_total / len(self.samples)), 3)  # save the whole dataset silhouette
         return clus_dict
 
     def compute_rand_index(self):
-        TP = 0
-        TN = 0
+        """
+        computes random index value(float) of the whole dataset
+        :return: returns the random index value(float) of the whole dataset
+        """
+        tp = 0
+        tn = 0
         number_of_pairs = math.factorial(len(self.samples)) / (2 * math.factorial(len(self.samples) - 2))
         for clus in self.clusters:
-            for sample in clus.samples:
-                for other in clus.samples:
+            for sample in clus.samples:  # iterate over every sample in the dataset
+                for other in clus.samples:  # iterate over every other sample in the same cluster
                     if sample.s_id >= other.s_id:
                         continue
-                    if sample.label == other.label:
-                        TP += 1
+                    if sample.label == other.label:  # two samples were clustered correctly into the same cluster
+                        tp += 1
 
-                for other_clus in self.clusters:
-                    for other in other_clus.samples:
-                        if other_clus == clus:
+                for other_clus in self.clusters:  # iterate over other clusters
+                    if other_clus == clus:
+                        continue
+                    for other in other_clus.samples:  # iterate over every other sample in other clusters
+                        if sample.s_id > other.s_id:  # avoid duplicate check
                             continue
-                        if sample.s_id > other.s_id:
-                            continue
-                        if sample.label != other.label:
-                            TN += 1
+                        if sample.label != other.label:  # two samples were clustered correctly into different clusters
+                            tn += 1
 
-        return round(float((TP + TN) / number_of_pairs), 3)
-
-    def compute_dist_clusters(self, merged_id, matrix):
-        dist_dict = {}
-        for clus in self.clusters:
-            for other_clus in self.clusters:
-                if (clus.c_id == merged_id or other_clus.c_id == merged_id) or (merged_id == -1):
-                    if (clus.c_id > other_clus.c_id):
-                        dist_dict[(clus.c_id, other_clus.c_id)] = self.link.compute(clus, other_clus, matrix)
-        return dist_dict
+        return round(float((tp + tn) / number_of_pairs), 3)
 
     def matrix_dist(self):
+        """
+        computes distances(float) between each pair of samples in the dataset
+        :return: returns a distances dictionary in format: key=(first_id, second_id), value=distance
+        """
         matrix_dic = {}
-        # for clus in self.clusters:
-        #     for other_clus in self.clusters:
-        #         if (clus.c_id > other_clus.c_id):
         for clus in self.clusters:
             for other_clus in self.clusters:
-                if (clus.samples[0].s_id > other_clus.samples[0].s_id):
-                    matrix_dic[(clus.samples[0].s_id, other_clus.samples[0].s_id)] = clus.samples[
-                        0].compute_euclidean_distance(other_clus.samples[0])
-
+                if clus.samples[0].s_id > other_clus.samples[0].s_id:  # avoid duplicates
+                    matrix_dic[(clus.samples[0].s_id, other_clus.samples[0].s_id)] = clus.samples[0]\
+                        .compute_euclidean_distance(other_clus.samples[0])
         return matrix_dic
 
     def run(self, max_clusters):
-
+        """
+        runs the agglomerative clustering on the dataset and prints its results
+        :param max_clusters: number of clusters to stop clustering
+        """
         sample_dist_matrix = self.matrix_dist()
         self.link.print_link()
-        # merged_id = -1
-        # clus_dist_matrix = self.compute_dist_clusters(merged_id, sample_dist_matrix)
-        # first_id = 0
-        # second_id = 0
-        first_clus = self.clusters[0]
-        second_clus = self.clusters[0]
-        min_dist = sample_dist_matrix[list(sample_dist_matrix.keys())[0]]
-        # min_dist=22222
-        while (len(self.clusters) > max_clusters):
-            for clus in self.clusters:
-                for other_clus in self.clusters:
-                    if (clus.c_id > other_clus.c_id):
+        first_clus = self.clusters[0]  # initialize first cluster to merge into
+        second_clus = self.clusters[0]  # initialize second cluster to merge
+        max_samples_dist = max(sample_dist_matrix.values())
+        # initialize minimun distance between two samples
+        min_dist = max_samples_dist
+        while len(self.clusters) > max_clusters:  # clustering loop
+            for clus in self.clusters:  # iterate over every cluster
+                for other_clus in self.clusters:  # iterate over other clusters
+                    if clus.c_id > other_clus.c_id:  # avoid duplicates and make sure to pass correct key to dictionary
+                        # compute distance between two clusters according to current link
                         clus_dist = self.link.compute(clus, other_clus, sample_dist_matrix)
-                        if clus_dist < min_dist:
+                        if clus_dist < min_dist:  # keep the minimum distance and its clusters
                             min_dist = clus_dist
                             first_clus = other_clus
                             second_clus = clus
-            self.clusters.remove(second_clus)
-            first_clus.merge(second_clus)
-            min_dist = max(sample_dist_matrix.values())
-
-            # for sample in clus.samples:
-            #     for other_sample in other_clus.samples:
-            #         if (sample.s_id > other_sample.s_id):
-            #             if (sample_dist_matrix[(sample.s_id, other_sample.s_id)] < min_dist):
-            #                 min_dist = sample_dist_matrix[(sample.s_id, other_sample.s_id)]
-            #                 if clus.c_id > other_clus.c_id:
-            #                     other_clus.merge(clus)
-            #                     # merged_id = other_clus.c_id
-            #                 else:
-            #                     clus.merge(other_clus)
-            #                     # merged_id = clus.c_id
-
-            # first_clus.merge(second_clus)
-            # for key in clus_dist_matrix.keys():
-            #     if clus_dist_matrix[key] < min_dist:
-            #         first_id = key[0]
-            #         second_id = key[1]
-            #         min_dist = clus_dist_matrix[key]
-            # for clus in self.clusters:
-            #     if clus.c_id == first_id:
-            #         first_clus = clus
-            #     if clus.c_id == second_id:
-            #         second_clus = clus
-            #
-            # if first_id > second_id:
-            #     merged_id = second_id
-            # else:
-            #     merged_id = first_id
-            # clus_dist_matrix = self.compute_dist_clusters(merged_id, sample_dist_matrix)
+            self.clusters.remove(second_clus)  # remove the cluster that's getting merged from clusters list
+            first_clus.merge(second_clus)  # merge the cluster with higher id into the other
+            min_dist = max_samples_dist  # restore high distance in order to start the search again
 
         sum_sil = self.compute_summery_silhouette(sample_dist_matrix)
+        # print results
         for clus in self.clusters:
             clus.print_details(sum_sil[clus.c_id])
         print(f'Whole data: silhouette = {sum_sil[0]}, RI = {self.compute_rand_index()}')
